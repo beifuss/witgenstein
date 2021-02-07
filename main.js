ada2ce139c5212b6480c5dea883c9111ec711b36
beifuss <esriechtnachwermut@gmail.com>
2021-02-07 23:40:26 +0900

    just played with page structure

---
+++ main.js
@@ -0,0 +1,14 @@
+function splitScroll() {
+    const controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({
+        duration: "200%",
+        triggerElement: ".about",
+        triggerHook: 0
+    })
+    .setPin(".about")
+    /*.addIndicators()*/
+    .addTo(controller);
+}
+
+splitScroll();
